c92a48dd79a3041e9ac045938dd337b81c2f4a50
aburk3 <aburk3@gmail.com>
2019-07-25 08:33:48 -0500

    Added Habits container and react-redux package

--- habit-tracker-client/src/containers/Habits.js
+++ habit-tracker-client/src/containers/Habits.js
@@ -5,3 +5,32 @@ import HabitCard from '../components/HabitCard';
 import HabitForm from './HabitForm';
 import { getHabits } from '../actions/habits';
 import './Habits.css';
+
+class Habits extends Component {
+  componentDidMount() {
+    this.props.getHabits();
+  }
+
+  render() {
+    return (
+      <div className="HabitsContainer">
+        <h1>Habits Component</h1>
+        {this.props.habits.map(habit => (
+          <HabitCard key={habit.id} habit={habit} />
+        ))}
+        <HabitForm />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    habits: state.habits
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getHabits }
+)(Habits);
