1f6d3781fa2a783b1af501d96a026e16b3ab8fdd
aburk3 <aburk3@gmail.com>
2019-07-25 09:32:26 -0500

    Added environment file for rails api backend and removed dollar sign on the HabitCard component

--- habit-tracker-client/src/components/HabitCard.js
+++ habit-tracker-client/src/components/HabitCard.js
@@ -3,8 +3,10 @@ import React from 'react';
 const HabitCard = ({ habit }) => (
   <div key={habit.id} className="HabitCard">
     <h3>{habit.name}</h3>
-    <p>Streak: ${habit.streak}</p>
-    <p>description: ${habit.description}</p>
+    <p>Streak: {habit.streak}</p>
+    <p>
+      <b>Description:</b> {habit.description}
+    </p>
   </div>
 );
 
