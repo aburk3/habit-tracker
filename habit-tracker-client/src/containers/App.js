fda57c85bfa659917b8394155cf4d045572bb946
aburk3 <aburk3@gmail.com>
2019-07-25 08:48:29 -0500

    Added Habit component to the App.js container

--- habit-tracker-client/src/containers/App.js
+++ habit-tracker-client/src/containers/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+
+import Habits from './Habits';
 import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App">App Container</div>;
+    return (
+      <div className="App">
+        <Habits />
+      </div>
+    );
   }
 }
 
