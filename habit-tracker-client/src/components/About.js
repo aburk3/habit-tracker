42aa1a737f29410af0bc2873bded957802526235
aburk3 <aburk3@gmail.com>
2019-07-27 13:55:36 -0500

    Added a short description to the About page

--- habit-tracker-client/src/components/About.js
+++ habit-tracker-client/src/components/About.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const About = () => {
-  return <h1>About this App</h1>;
+  return (
+    <>
+      <h1>About this App</h1>
+      <div>
+        The intention of this application is to allow a user to easily track
+        habits. First, you can add a new habit that has a name and description.
+        Next, each time you complete/perform the habit, you can increment the
+        habit as desired to track your current 'habit-streak'.
+      </div>
+    </>
+  );
 };
 
 export default About;
